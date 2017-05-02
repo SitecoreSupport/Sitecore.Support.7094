0452b4a0cffc84edd3956d9b7a8d56bfd29c1252
Evgen Savinov <evs@sitecore.net>
2017-05-02 11:19:32 +0300

    Initial commit

---
+++ src/Sitecore.Support.7094/SCSupportLookupNameLookupValue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using Sitecore.Text;
+
+namespace Sitecore.Support
+{
+    public class LookupNameLookupValue: Sitecore.XA.Foundation.SitecoreExtensions.CustomFields.FieldTypes.LookupNameLookupValue
+    {
+        protected override void LoadValue()
+        {
+            if (!this.ReadOnly && !this.Disabled)
+            {
+                System.Web.UI.Page handler = HttpContext.Current.Handler as System.Web.UI.Page;
+                NameValueCollection values = (handler != null) ? handler.Request.Form : new NameValueCollection();
+                UrlString str = new UrlString();
+                
+                foreach (string str2 in values.Keys)
+                {
+                    if ((!string.IsNullOrEmpty(str2) && str2.StartsWith(this.ID + "_Param")) && !str2.EndsWith("_value"))
+                    {
+                        string str3 = values[str2];
+                        string str4 = values[str2 + "_value"];
+                        if (!string.IsNullOrEmpty(str3))
+                        {
+                            str[str3] = str4 ?? string.Empty;
+                        }
+                    }
+                }
+                //fix for the Bug 7094
+                string decodedValue = HttpUtility.UrlDecode(str.ToString());
+                    if (this.Value != decodedValue)
+                    {
+                        this.Value = decodedValue;
+                        this.SetModified();
+                    }
+            }
+        }
+    }
+}
